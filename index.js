1da4ff0822afe5aebab7f40bf2f32806ae2a9da0
Samuel <samkoj@hotmail.co.uk>
2021-05-21 18:51:14 +0100

    use app.route() instead of individual request type handlers

--- index.js
+++ index.js
@@ -10,41 +10,40 @@ app.use(express.static("public"));
 mongoose.connect("mongodb+srv://admin-samuel:Test123@cluster0.lpl8d.mongodb.net/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const articleSchema = mongoose.Schema({
-    title: String,
-    content: String
+	title: String,
+	content: String
 });
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function (req, res) {
-	Article.find({}, function (err, docs) {
-		res.send(docs);
-	}).catch((err) => console.error(err));
-});
-
-app.post("/articles", function (req, res) {
-	const newArticle = new Article({
-		title: req.body.title,
-		content: req.body.content,
+app.route("/articles")
+	.get(function (req, res) {
+		Article.find({}, function (err, docs) {
+			res.send(docs);
+		}).catch((err) => console.error(err));
+	})
+	.post(function (req, res) {
+		const newArticle = new Article({
+			title: req.body.title,
+			content: req.body.content,
+		});
+		newArticle.save(function (err) {
+			if (!err) {
+				res.send("Successfully added a new article.");
+			} else {
+				res.send(err);
+			}
+		});
+	})
+	.delete(function (req, res) {
+		Article.deleteMany({}, function (err) {
+			if (!err) {
+				res.send("Successfully deleted all articles.");
+			} else {
+				res.send(err);
+			}
+		});
 	});
-	newArticle.save(function (err) {
-		if (!err) {
-			res.send("Successfully added a new article.");
-		} else {
-			res.send(err);
-		}
-	});
-});
-
-app.delete("/articles", function (req, res) {
-	Article.deleteMany({}, function (err) {
-        if (!err) {
-            res.send("Successfully deleted all articles.");
-        } else {
-            res.send(err);
-        }
-    });
-});
 
 app.listen(3000, function () {
 	console.log(`Server started on port 3000.`);
